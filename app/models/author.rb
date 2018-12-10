07f6bdd169f9ed2a3b13e268db2effebd07ef3a5
SyntheticAutomation <mclampet@msudenver.edu>
2018-12-09 18:02:29 -0700

    Adds author method to clean up input

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,16 @@ class Author < ApplicationRecord
   validates_presence_of :name
   has_many :book_authors
   has_many :books, through: :book_authors
+
+
+  def self.clean_me_up(params)
+    #params = "Hingle McKringleberry", "Donkey Teeth"
+    author_names = params[:authors].split(', ')
+    fixed_params = author_names.map do |author|
+      cleaned_name = author.downcase.titlecase
+      Author.find_or_create_by(name: cleaned_name)
+    end
+    params[:authors] = fixed_params
+    params
+  end
 end
