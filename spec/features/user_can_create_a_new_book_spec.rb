fee599b42649734a87c1116d4e005923cc2e3e4c
SyntheticAutomation <mclampet@msudenver.edu>
2018-12-09 20:04:45 -0700

    Adds a test to validate if new book entry is duplicate

--- spec/features/user_can_create_a_new_book_spec.rb
+++ spec/features/user_can_create_a_new_book_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe 'a user who visits our web app' do
   it 'creates a book' do
-
     visit books_path
 
     click_link 'Add Book'
 
     expect(current_path).to eq(new_book_path)
+
     title = "Burgers and Milkshakes"
     pages = 190
     published_year = 2015
@@ -24,5 +24,20 @@ describe 'a user who visits our web app' do
     expect(page).to have_content(pages)
     expect(page).to have_content(published_year)
     expect(current_path).to eq("/books/#{Book.last.id}")
+  end
+  it 'goes to new book page if title already exists in the library' do
+    book_1 = Book.create(title: "War and Peace", pages: 293, published_year: 1983)
+    author_1 = book_1.authors.create(name: "Thomas Christie")
+
+    visit new_book_path
+
+    title = "War and Peace"
+    pages = 190
+    published_year = 2015
+    authors = "Barney the Dinosaur"
+
+    click_on "Create Book"
+    
+    expect(page).to have_content("Unable to add new book. The Library may already contain this book or its author.")
   end
 end
