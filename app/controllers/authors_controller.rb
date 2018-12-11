e512c1171026622b3883fecc713d65380cb123da
Lance Taylor <lance.taylor98@hotmail.com>
2018-12-11 07:55:49 -0700

    adds destroy method to controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,4 +6,14 @@ class AuthorsController < ApplicationController
       redirect books_path
     end
   end
+
+  def destroy
+    author = Author.find(params[:id])
+    books = author.books
+    books.each do |book|
+      book.destroy
+    end
+    author.destroy
+    redirect_to books_path
+  end
 end
