a1354e3ede66f4198f530eb908640857ccff7561
SyntheticAutomation <mclampet@msudenver.edu>
2018-12-11 14:13:59 -0700

    Adds test for user_name being linked, and top/bottom reviews for book show

--- spec/features/user_can_see_book_show_page_spec.rb
+++ spec/features/user_can_see_book_show_page_spec.rb
@@ -2,16 +2,26 @@ require 'rails_helper'
 
 describe 'book_show' do
   it 'user_can_see_a_single_book' do
+    user_1 = User.create(name: "NAMABYDE")
     book_1 = Book.create(title: "Of Mice and Men",  pages: 170, published_year: 1932)
     book_1.authors.create(name: "Jimmy Smit")
+    review_1 = book_1.reviews.create(title: "KDFJLDSJF", description: "tjskljaejfkjdfl;ewioefdfdjs", rating: 1, user_id: user_1.id)
+    review_2 = book_1.reviews.create(title: "rev_2", description: "hellol;ewioefdfdjs", rating: 2,  user_id: user_1.id)
+    review_3 = book_1.reviews.create(title: "rev_3", description: "hellol;ewioefdfdjs", rating: 3,  user_id: user_1.id)
+    review_4 = book_1.reviews.create(title: "rev_4", description: "hellol;ewioefdfdjs", rating: 4,  user_id: user_1.id)
+    review_5 = book_1.reviews.create(title: "rev_5", description: "hellol;ewioefdfdjs", rating: 5,  user_id: user_1.id)
 
     visit book_path(book_1)
 
-    within "#book-information"
+    within "#book-information" do
       expect(page).to have_content(book_1.title)
       expect(page).to have_content(book_1.pages)
       expect(page).to have_content(book_1.published_year)
       expect(page).to have_content(book_1.authors[0].name)
+    end
+    within ".book-stats" do
+      expect(page).to have_content(review_5.title)
+    end
   end
 
   it 'user_can_see_all_reviews_for_this_book' do
@@ -33,6 +43,7 @@ describe 'book_show' do
       expect(page).to have_content(book_1.reviews[1].user_name(user_2.id))
       expect(page).to have_content(book_1.reviews[1].rating)
       expect(page).to have_content(book_1.reviews[1].description)
+
   end
 
   it 'user_can_click_a_user_link_to_user_show_page' do
@@ -42,8 +53,10 @@ describe 'book_show' do
     book_1.reviews.create(title: "KDFJLDSJF", description: "tjskljaejfkjdfl;ewioefdfdjs", rating: 1, user_id: user_1.id)
 
     visit book_path(book_1)
-    
-    click_on (user_1.name)
+
+    within "#book-reviews" do
+      click_on (user_1.name)
+    end
 
     expect(page).to have_current_path(user_path(user_1))
   end
